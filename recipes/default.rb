94352050fe5fc9410df0f201a0e0a4f461a7f315
upcFrost <upcfrost@gmail.com>
2016-04-04 10:57:47 -0400

    Fixed the npm installation

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,13 +8,20 @@
 #
 
 node.default['nodejs']['install_method'] = 'binary'
-node.default['nodejs']['npm']['install_method'] = 'binary'
+node.default['nodejs']['npm']['install_method'] = 'embedded'
 node.default['nodejs']['version'] = '5.10.0'
 node.default['nodejs']['binary']['checksum']['linux_x64'] = 'a458ddab5f8d071c9b4f24ccfa685aedd57ccf7338c3ea0e2b99546cf35a3958'
 node.default['nodejs']['binary']['checksum']['linux_x86'] = 'e884f070542f49f577fd9785f09cf7734e6c2107d23b6c1b58453d43183c5ec0'
 
 # NodeJS install
 include_recipe 'nodejs'
+
+# Set NodeJS prefix
+# We can adjust PATH here also, but this way is imo better
+execute 'set_nodejs_prefix' do
+  command 'npm config set prefix /usr/local'
+  action :nothing
+end
 
 # Check that git is installed
 include_recipe 'git'
