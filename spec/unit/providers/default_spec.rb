b6aa7342484c1e14a18693c85a3ea0f74a5ac786
upcFrost <upcfrost@gmail.com>
2016-05-09 14:19:30 +0300

    Added more simple checks

--- spec/unit/providers/default_spec.rb
+++ spec/unit/providers/default_spec.rb
@@ -9,5 +9,17 @@ describe 'test::default_provider' do
     it 'should create systemd service template' do
       expect(chef_run).to create_template('/lib/systemd/system/docker-etcd-registrator.service')
     end
+
+    it 'should enable the new service' do
+      expect(chef_run).to enable_service('docker-etcd-registrator')
+    end
+
+    it 'should create the config dir' do
+      expect(chef_run).to create_directory('/etc/docker-etcd-registrator')
+    end
+
+    it 'should write the default config file' do
+      expect(chef_run).to create_template('/etc/docker-etcd-registrator/docker-etcd-registrator.conf')
+    end
   end
 end
